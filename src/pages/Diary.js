b0d6ccb294917b1748b18c1bf44dda58a72882e6
Jaeyoon1991 <rainbowcrayon91@gmail.com>
2025-02-10 23:52:39 +0900

    리액트 라우터로 동적 경로 라우팅하기

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,4 +1,14 @@
+import { useParams} from "react-router-dom";
+
 const Diary = () => {
-    return <div>Diary 페이지 입니다.</div>
+    // url경로에 포함된 파라미터를 객체로 변환
+    const { id } = useParams();
+    return (
+        <div>
+            <div>{id}번 일기</div>
+            <div>Diary 페이지 입니다.</div>
+        </div>
+    
+    )
 };
 export default Diary;
